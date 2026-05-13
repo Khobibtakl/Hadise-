753da4bc48a6a8e0cc33c61d33e066ba0a2246ff
Khobibtakl <152937380+Khobibtakl@users.noreply.github.com>
2026-05-14 01:18:14 +0430

    capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.hadith.app',
+  appId: 'khubaibtakl.hadise.af',
   appName: 'حديثونه',
   webDir: 'dist',
   plugins: {
